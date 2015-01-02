578f956ab5f06c0d05ea71aa69f678ddb56cd27f
Mohab Usama <mohab.usama@gmail.com>
2015-01-02 22:59:15 +0200

    Fix import

--- users_api/tests/__init__.py
+++ users_api/tests/__init__.py
@@ -1,3 +1,3 @@
-from users import UsersResourceTest  # noqa
-from groups import GroupsResourceTest  # noqa
-from permissions import PermissionsResourceTest  # noqa
+from .users import UsersResourceTest  # noqa
+from .groups import GroupsResourceTest  # noqa
+from .permissions import PermissionsResourceTest  # noqa
